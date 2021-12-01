a3a170b1f5b244ba1f71901669bbb859c5055fed
Dragnansia <50181744+Dragnansia@users.noreply.github.com>
2021-12-02 00:55:53 +0100

    fix color with command download

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-echo "\x1b[0;32m[Info] Download last opengame release\x1b[0m"
+echo -e "\x1b[0;32m[Info] Download last opengame release\x1b[0m"
 curl -o opengame -LJO "$(curl -s https://api.github.com/repos/Dragnansia/OpenGame/releases | grep browser_download_url | grep 'opengame' | head -n 1 | cut -d '"' -f 4)"
 
-echo "\x1b[0;32m[Info] Add execution mode to opengame\x1b[0m"
+echo -e "\x1b[0;32m[Info] Add execution mode to opengame\x1b[0m"
 chmod +x opengame
 
-echo "\x1b[0;32m[Info] Move opengame to ~/bin\x1b[0m"
+echo -e "\x1b[0;32m[Info] Move opengame to ~/bin\x1b[0m"
 mv opengame ~/bin
