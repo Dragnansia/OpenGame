02716caf7d759a9e37752351065cb268470132b0
Dragnansia <50181744+Dragnansia@users.noreply.github.com>
2021-12-02 00:39:08 +0100

    fix download file with installer.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Download last release
-curl -o opengame -LJO $(curl -s https://api.github.com/repos/Dragnansia/OpenGame/releases | grep browser_download_url | grep '[.]deb' | head -n 1 | cut -d '\"' -f 4)
+curl -o opengame -LJO "$(curl -s https://api.github.com/repos/Dragnansia/OpenGame/releases | grep browser_download_url | grep 'opengame' | head -n 1 | cut -d '"' -f 4)"
 
 # Add Execution
 chmod +x opengame
