d4429ae1e0c47be831bad2c540bfe1ffbe0c0b63
Dragnansia <50181744+Dragnansia@users.noreply.github.com>
2021-12-05 15:24:31 +0100

    use vec! macro instead of to_vec

--- src/pckg/arch.rs
+++ src/pckg/arch.rs
@@ -6,19 +6,19 @@ pub struct Arch {}
 // TODO: Need a verification on a arch distribution
 impl Installer for Arch {
     fn all(&self, root: &String) -> Vec<String> {
-        [
+        vec![
             format!("{} pacman -Syu python-pip wine-staging winetricks lutris steam gamemode giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader -y --needed --noconfirm", root),
-        ].to_vec()
+        ]
     }
 
     fn gaming(&self, root: &String) -> Vec<String> {
-        [
+        vec![
             format!("{} pacman -Syu python-pip wine-staging winetricks steam gamemode giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader -y --needed --noconfirm", root),
-        ].to_vec()
+        ]
     }
 
     fn lutris(&self, root: &String) -> Vec<String> {
-        [format!("{} pacman -Syu lutris", root)].to_vec()
+        vec![format!("{} pacman -Syu lutris", root)]
     }
 
     fn heroic_launcher(&self, _root: &String) -> Vec<String> {
@@ -26,49 +26,45 @@ impl Installer for Arch {
 
         if aur.is_empty() {
             error!("No Aur package manager found for this arch distro");
-            Vec::new()
+            vec![]
         } else {
-            [format!(
+            vec![format!(
                 "{} -S heroic-games-launcher-bin -y --needed --noconfirm",
                 aur
             )]
-            .to_vec()
         }
     }
 
     fn overlay(&self, _root: &String) -> Vec<String> {
         let aur = get_aur_package_manager();
 
-        match aur.is_empty() {
-            true => {
-                error!("No Aur package manager found for this arch distro");
-                Vec::new()
-            }
-            false => [format!("{} goverlay-bin -y --needed --noconfirm", aur)].to_vec(),
+        if aur.is_empty() {
+            error!("No Aur package manager found for this arch distro");
+            vec![]
+        } else {
+            vec![format!("{} goverlay-bin -y --needed --noconfirm", aur)]
         }
     }
 
     fn replay_sorcery(&self, _root: &String) -> Vec<String> {
         let aur = get_aur_package_manager();
 
-        match aur.is_empty() {
-            true => {
-                error!("No Aur package manager found for this arch distro");
-                Vec::new()
-            }
-            false => [format!("{} replay-sorcery -y --needed --noconfirm", aur)].to_vec(),
+        if aur.is_empty() {
+            error!("No Aur package manager found for this arch distro");
+            vec![]
+        } else {
+            vec![format!("{} replay-sorcery -y --needed --noconfirm", aur)]
         }
     }
 
     fn mini_galaxy(&self, _root: &String) -> Vec<String> {
         let aur = get_aur_package_manager();
 
-        match aur.is_empty() {
-            true => {
-                error!("No Aur package manager found for this arch distro");
-                Vec::new()
-            }
-            false => [format!("{} minigalaxy -y --needed --noconfirm", aur)].to_vec(),
+        if aur.is_empty() {
+            error!("No Aur package manager found for this arch distro");
+            vec![]
+        } else {
+            vec![format!("{} minigalaxy -y --needed --noconfirm", aur)]
         }
     }
 }
--- src/pckg/ubuntu.rs
+++ src/pckg/ubuntu.rs
@@ -5,7 +5,7 @@ pub struct Ubuntu {}
 
 impl Installer for Ubuntu {
     fn all(&self, root: &String) -> Vec<String> {
-        [
+        vec![
             format!("{} apt install -y software-properties-common", root),
             format!("{} dpkg --add-architecture i386", root),
             "wget -nc https://dl.winehq.org/wine-builds/winehq.key".to_string(),
@@ -25,11 +25,10 @@ impl Installer for Ubuntu {
             ),
             format!("{} apt install -y steam goverlay winetricks lutris python3-pip gawk curl meson libsystemd-dev pkg-config ninja-build git libdbus-1-dev libinih-dev dbus-user-session libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386", root),
         ]
-        .to_vec()
     }
 
     fn gaming(&self, root: &String) -> Vec<String> {
-        [
+        vec![
             format!("{} apt install -y software-properties-common", root),
             format!("{} dpkg --add-architecture i386", root),
             "wget -nc https://dl.winehq.org/wine-builds/winehq.key".to_string(),
@@ -46,44 +45,42 @@ impl Installer for Ubuntu {
                 root
             ),
             format!("{} apt install -y steam winetricks python3-pip gawk curl meson libsystemd-dev pkg-config ninja-build git libdbus-1-dev libinih-dev dbus-user-session libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386", root),
-        ].to_vec()
+        ]
     }
 
     fn lutris(&self, root: &String) -> Vec<String> {
-        [
+        vec![
             format!("{} apt install -y software-properties-common", root),
             format!("{} dpkg --add-architecture i386", root),
             format!("{} add-apt-repository -y ppa:lutris-team/lutris", root),
             format!("{} apt update", root),
             format!("{} apt install -y lutris", root),
         ]
-        .to_vec()
     }
 
     fn heroic_launcher(&self, _root: &String) -> Vec<String> {
         match dir::user_dir() {
-            Ok(ud) => [
+            Ok(ud) => vec![
                 "curl -o heroic.AppImage -LJO $(curl -s https://api.github.com/repos/Heroic-Games-Launcher/HeroicGamesLauncher/releases | grep browser_download_url | grep '[.]AppImage' | head -n 1 | cut -d '\"' -f 4)".to_string(),
                 "chmod +x heroic.AppImage".to_string(),
                 format!("mkdir -p {}/.applications", ud),
                 format!("mv heroic.AppImage {}/.applications", ud),
                 format!("{}/.applications/heroic.AppImage", ud)
-            ].to_vec(),
+            ],
             Err(err) => {
                 error!("{}", err);
-                Vec::new()
+                vec![]
             }
         }
     }
 
     fn overlay(&self, root: &String) -> Vec<String> {
-        [
+        vec![
             format!("{} dpkg --add-architecture i386", root),
             format!("{} add-apt-repository -y ppa:flexiondotorg/mangohud", root),
             format!("{} apt update", root),
             format!("{} apt install -y goverlay", root),
         ]
-        .to_vec()
     }
 
     fn replay_sorcery(&self, root: &String) -> Vec<String> {
@@ -92,7 +89,7 @@ impl Installer for Ubuntu {
             dir::format_tmp_dir("gaming", true).unwrap_or_default()
         );
 
-        [
+        vec![
             format!(
                 "{} apt install git cmake ffmpeg ffmpeg-libs ffmpeg libdrm libX11 libX11-xcb libX11",
                 root
@@ -110,12 +107,11 @@ impl Installer for Ubuntu {
             "systemctl --user enable --now replay-sorcery".to_string(),
             format!("{} systemctl enable --now replay-sorcery-kms", root),
         ]
-        .to_vec()
     }
 
     fn mini_galaxy(&self, root: &String) -> Vec<String> {
         match dir::user_dir() {
-            Ok(dir) => [
+            Ok(dir) => vec![
                 format!(
                     "curl -o {}/minigalaxy.deb -LJO {}",
                     dir,
@@ -123,11 +119,10 @@ impl Installer for Ubuntu {
                 ),
                 format!("{} apt install {}/minigalaxy.deb", root, dir),
                 format!("{} rm -f {}/minigalaxy.deb", root, dir),
-            ]
-            .to_vec(),
+            ],
             Err(err) => {
                 error!("{}", err);
-                Vec::new()
+                vec![]
             }
         }
     }
