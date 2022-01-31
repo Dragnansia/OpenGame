0d35ebb55eb3c353e2d21e6dbcf9d58103dcbae5
Dragnansia <50181744+Dragnansia@users.noreply.github.com>
2022-01-31 02:59:29 +0100

    update clap to 3.0 version

---
+++ src/arguments.rs
@@ -0,0 +1,77 @@
+use clap::{AppSettings, Args, Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(author, version, about, long_about = None)]
+#[clap(global_setting(AppSettings::PropagateVersion))]
+#[clap(global_setting(AppSettings::UseLongFormatForHelpSubcommand))]
+pub struct Cli {
+    #[clap(subcommand)]
+    pub commands: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// ProtonGE management (Install / Remove / List)
+    Proton(Proton),
+
+    /// Install gaming dependencies, install all dependencies if no FLAGS are added
+    Gaming(Gaming),
+}
+
+#[derive(Args)]
+pub struct Proton {
+    #[clap(short, long)]
+    /// Install a specific version of ProtonGE
+    pub install: Option<String>,
+
+    #[clap(short, long)]
+    /// Install latest ProtonGE version if not already install
+    pub update: bool,
+
+    #[clap(short, long)]
+    /// Install archive with the path
+    pub archive: Option<String>,
+
+    #[clap(short, long)]
+    /// Print all versions of Proton installs
+    pub list: bool,
+
+    #[clap(short, long)]
+    /// Remove a specific version
+    pub remove: Option<String>,
+
+    #[clap(short, long)]
+    /// Remove the cache directory of ProtonGE
+    pub clean: bool,
+}
+
+#[derive(Args)]
+pub struct Gaming {
+    #[clap(short, long)]
+    /// Install all dependencies
+    pub all: bool,
+
+    #[clap(short, long)]
+    /// Install gaming dependencies
+    pub gaming: bool,
+
+    #[clap(short, long)]
+    /// Install Lutris
+    pub lutris: bool,
+
+    #[clap(short = 'e', long)]
+    /// Install Heroic Launcher
+    pub heroic: bool,
+
+    #[clap(short, long)]
+    /// Install Overlay
+    pub overlay: bool,
+
+    #[clap(short, long)]
+    /// Install ReplaySorcery
+    pub replay_sorcery: bool,
+
+    #[clap(short, long)]
+    /// Install MiniGalaxy
+    pub mini_galaxy: bool,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,186 +1,92 @@
-#![feature(allow_internal_unstable)]
-
+mod arguments;
 mod dir;
 mod downloader;
 mod log;
-mod timer;
 mod pckg;
 mod proton;
 mod steam;
+mod timer;
 
-use clap::{App, Arg, ArgMatches, SubCommand};
-use pckg::{installer, run_commands};
+use arguments::{Cli, Commands};
+use clap::StructOpt;
+use pckg::{
+    installer::{self},
+    run_commands,
+};
+use proton::update_protonge;
 use steam::Steam;
-
-fn matches_argument() -> ArgMatches<'static> {
-    App::new("og")
-        .version("0.0.1")
-        .about("A simple program to install gaming dependencies on Linux computer")
-        .subcommand(
-            SubCommand::with_name("proton")
-                .about("ProtonGE management (Install / Remove / List)")
-                .arg(
-                    Arg::with_name("install")
-                        .short("i")
-                        .long("install")
-                        .help("Install a specific version of ProtonGE")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("update")
-                        .short("u")
-                        .long("update")
-                        .help("Install latest ProtonGE version")
-                        .takes_value(false),
-                )
-                .arg(
-                    Arg::with_name("archive")
-                        .short("a")
-                        .long("archive")
-                        .help("Install archive with the path")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("list")
-                        .short("l")
-                        .long("list")
-                        .help("Print all versions of Proton installs")
-                        .takes_value(false),
-                )
-                .arg(
-                    Arg::with_name("remove")
-                        .short("r")
-                        .long("remove")
-                        .help("Remove a specific version")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("clean")
-                        .short("c")
-                        .long("clean")
-                        .help("Remove the cache directory of ProtonGE")
-                        .takes_value(false),
-                ),
-        )
-        .subcommand(
-            SubCommand::with_name("gaming")
-                .about(
-                    "Install gaming dependencies, install all dependencies if no FLAGS are added",
-                )
-                .arg(
-                    Arg::with_name("gaming")
-                        .short("g")
-                        .long("gaming")
-                        .help("Install gaming dependencies")
-                        .takes_value(false),
-                )
-                .arg(
-                    Arg::with_name("lutris")
-                        .short("l")
-                        .long("lutris")
-                        .help("Install Lutris")
-                        .takes_value(false),
-                )
-                .arg(
-                    Arg::with_name("heroic")
-                        .short("h")
-                        .long("heroic")
-                        .help("Install Heroic Launcher")
-                        .takes_value(false),
-                )
-                .arg(
-                    Arg::with_name("overlay")
-                        .short("o")
-                        .long("overlay")
-                        .help("Install Overlay")
-                        .takes_value(false),
-                )
-                .arg(
-                    Arg::with_name("replay-sorcery")
-                        .short("r")
-                        .long("replay-sorcery")
-                        .help("Install ReplaySorcery")
-                        .takes_value(false),
-                )
-                .arg(
-                    Arg::with_name("minigalaxy")
-                        .short("m")
-                        .long("minigalaxy")
-                        .help("Install MiniGalaxy")
-                        .takes_value(false),
-                ),
-        )
-        .get_matches()
-}
 
 #[tokio::main]
 async fn main() {
-    let matches = matches_argument();
+    let args = Cli::parse();
 
-    if let Some(matches) = matches.subcommand_matches("proton") {
-        match Steam::new() {
-            Ok(steam) => {
-                if matches.is_present("install") {
-                    proton::install_version(matches.value_of("install").unwrap(), &steam).await;
+    match &args.commands {
+        Commands::Proton(proton) => {
+            let steam = Steam::new();
+            if let Ok(steam) = steam {
+                if let Some(v) = &proton.install {
+                    proton::install_version(v, &steam).await;
                 }
 
-                if matches.is_present("remove") {
-                    proton::remove_version(matches.value_of("remove").unwrap(), &steam);
+                if proton.update {
+                    update_protonge(&steam).await;
                 }
 
-                if matches.is_present("list") {
+                if let Some(p) = &proton.archive {
+                    proton::install_archive_version(p, &steam);
+                }
+
+                if proton.list {
                     proton::list_version(&steam);
                 }
-
-                if matches.is_present("archive") {
-                    proton::install_archive_version(matches.value_of("archive").unwrap(), &steam);
+                if let Some(v) = &proton.remove {
+                    proton::remove_version(v, &steam)
                 }
-
-                if matches.is_present("update") {
-                    proton::update_protonge(&steam).await;
-                }
-
-                if matches.is_present("clean") {
+                if proton.clean {
                     proton::remove_cache();
                 }
+            } else {
+                error!("Steam initialisation error: {}", steam.err().unwrap())
             }
-            Err(e) => error!("Steam initialisation error: {}", e),
         }
-    }
+        Commands::Gaming(gaming) => match installer::find_installer() {
+            Ok(cmds) => {
+                let root = installer::root_command();
+                let mut commands = vec![];
 
-    if let Some(matches) = matches.subcommand_matches("gaming") {
-        let root = installer::root_command();
-        match installer::find_installer() {
-            Ok(commands) => {
-                if matches.args.len() == 0 {
-                    run_commands(&commands.all(&root).await);
+                if gaming.all {
+                    commands.append(&mut cmds.all(&root));
                 } else {
-                    if matches.is_present("gaming") {
-                        run_commands(&commands.gaming(&root).await);
+                    if gaming.gaming {
+                        commands.append(&mut cmds.gaming(&root));
                     }
 
-                    if matches.is_present("lutris") {
-                        run_commands(&commands.lutris(&root).await);
+                    if gaming.lutris {
+                        commands.append(&mut cmds.lutris(&root));
                     }
 
-                    if matches.is_present("heroic") {
-                        run_commands(&commands.heroic_launcher(&root).await);
+                    if gaming.heroic {
+                        commands.append(&mut cmds.heroic_launcher(&root));
                     }
 
-                    if matches.is_present("overlay") {
-                        run_commands(&commands.overlay(&root).await);
+                    if gaming.overlay {
+                        commands.append(&mut cmds.overlay(&root));
                     }
 
-                    if matches.is_present("replay-sorcery") {
-                        run_commands(&commands.replay_sorcery(&root).await);
+                    if gaming.replay_sorcery {
+                        commands.append(&mut cmds.replay_sorcery(&root));
                     }
 
-                    if matches.is_present("minigalaxy") {
-                        run_commands(&commands.mini_galaxy(&root).await);
+                    if gaming.mini_galaxy {
+                        commands.append(&mut cmds.mini_galaxy(&root));
                     }
                 }
+
+                run_commands(&commands);
             }
-            Err(err) => error!("{}", err.to_string()),
-        }
+            Err(err) => {
+                error!("{}", err.to_string());
+            }
+        },
     }
 }
