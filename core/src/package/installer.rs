bc51e488c2cee3e441b20e5fc4f83bb6990c9a00
Dragnansia <romuald.auc.git@protonmail.com>
2022-05-17 21:27:16 +0200

    add more function on Installer and PackageManager

--- core/src/package/installer.rs
+++ core/src/package/installer.rs
@@ -1,22 +1,42 @@
 //! Used to install package from package manager
+use crate::os::release_data;
 use serde::Deserialize;
-use std::collections::HashMap;
+use std::io;
 
-/// All function to install package on distro
-/// are saved on HashMap
+/// All function to install gaming dependencies or package
+/// on distro
 #[derive(Debug, Deserialize)]
 pub struct Installer {
     pub distro_name: String,
-
-    /// Package Name - Commands
-    pub packages: HashMap<String, Vec<String>>,
 }
 
 impl Installer {
-    fn new() -> Self {
-        Self {
-            distro_name: String::new(),
-            packages: HashMap::new(),
-        }
+    /// Create a new Installer for package
+    ///
+    /// Not found current distro name is a possibility
+    pub fn new() -> io::Result<Self> {
+        Ok(Self {
+            distro_name: Self::found_disto_name()?,
+        })
+    }
+
+    /// Return all commands necessary to install package
+    /// and format to used package manager to install
+    ///
+    /// Need to used with other function to run all commands
+    ///
+    /// # Exemple
+    /// ```
+    /// use core::package::installer::Installer;
+    ///
+    /// let commands = Installer::new().unwrap().get_commands("steam");
+    /// ```
+    pub fn get_commands(&self, group_name: &str) -> Vec<String> {
+        todo!()
+    }
+
+    /// Return current distro name
+    fn found_disto_name() -> io::Result<String> {
+        release_data("NAME")
     }
 }
--- core/src/package/pm.rs
+++ core/src/package/pm.rs
@@ -2,3 +2,38 @@
 //! current distro, and call basic command
 //!
 //! Can be used to get package installed on current distro
+use std::{io, path::Path};
+
+static PACKAGES_MANAGER: [&str; 4] = ["dnf", "pacman", "apt", "yum"];
+
+/// Return current
+pub struct PackageManager {
+    name: &'static str,
+}
+
+impl PackageManager {
+    /// Find current package manager installed
+    pub fn new() -> io::Result<Self> {
+        Ok(Self {
+            name: Self::found_package_manager()?,
+        })
+    }
+
+    /// Return current installed package manager
+    /// found on the system
+    fn found_package_manager() -> io::Result<&'static str> {
+        if let Some(pm) = PACKAGES_MANAGER
+            .into_iter()
+            .find(|&pm| Path::new(&format!("/usr/bin/{pm}")).exists())
+        {
+            return Ok(pm);
+        }
+
+        Err(io::Error::new(io::ErrorKind::NotFound, "Package manager"))
+    }
+
+    /// Install all packages
+    pub fn install_packages<T: AsRef<str>>(&self, commands: &[T]) {
+        todo!()
+    }
+}
